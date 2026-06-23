a1275dc30405837fe50d09fda17802b7854cd4e1
Rene Kolednik <kolednik.rene@scptuj.si>
2026-06-23 17:31:30 +0200

    Add Integer and Float Numeric Operations demonstration; include various calculations

---
+++ practical/CS50P/Variables_Functions/Intiger_Float_Numeric.py
@@ -0,0 +1,23 @@
+#as we now have figured out there are multiple types in python
+#we have:
+x = 3.13    #float
+y = 34      #integer
+z = 2
+
+
+#eventualy we can also execute verious numeric calculations such as:
+
+deljenje = y / x
+print(deljenje)
+
+mnozenje = y * x
+print(mnozenje)
+
+sestevanje = y + x
+print(sestevanje)
+
+odstevanje = y - x
+print(odstevanje)
+
+celostevilsko_deljenje = y % z
+print(celostevilsko_deljenje)
