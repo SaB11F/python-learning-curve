55273e7150835fbfcb3cd1b9897bb6737f94e376
Rene Kolednik <kolednik.rene@scptuj.si>
2026-06-23 16:56:53 +0200

    Enhance String methods demonstration; add inline examples for title and strip methods

--- practical/CS50P/Variables_Functions/String_methods.py
+++ practical/CS50P/Variables_Functions/String_methods.py
@@ -23,4 +23,14 @@ m = m.title()
 print(m)
 
 #To je bilo zgolj par izmed metod, ki jih string prenese
-#več metod si lahko pogledaš tukaj - https://www.w3schools.com/python/python_strings.asp
+#več metod si lahko pogledaš tukaj - https://www.w3schools.com/python/python_strings.aspž
+
+#Imamo še par fint, ki jih lahko uporabimo pri klicanju metod in sicer:
+
+#tukaj smo kar inline dodali metodo pri sami deklaraciji vrednosti spremenljivke
+p = "rene kolednik".title()
+print(f"to je p: {p}")
+
+#ista finta, eventualy lahko tukaj dodamo kolikor metod želimo, vse inline zato, da je bolj pregledno.
+r = "    rene kolednik   ".strip().title()
+print(f"tukaj imamo r: {r}")
