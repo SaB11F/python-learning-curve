5ce8136a62a80e8104850cac5f9b498b0f3ae827
Rene Kolednik <kolednik.rene@scptuj.si>
2026-06-26 10:51:51 +0200

    Add simple while loop demonstration with basic iteration example

---
+++ practical/CS50P/Loops/while.py
@@ -0,0 +1,9 @@
+#tukaj je preprosti while loop
+#koncept je preprost / isti kot v c++
+#Loop se izvaja tako dolgo dokler ne zagotovi pogoja, ki ga predpostavimo
+#Loop se izvede minimalno 1x
+
+i = 0
+while i < 5:
+    print(i+1)
+    i = i + 1
